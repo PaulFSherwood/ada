b785ea857babc35d292714e75ee74352304cc286
PaulFSherwood <paulfsherwood@gmail.com>
2022-01-03 17:19:32 -0700

    adding more lessons from adacore.pdf

--- learnAda01/check_direction.adb
+++ learnAda01/check_direction.adb
@@ -2,31 +2,33 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure Check_Direction is
-    N : Integer;
+    N : integer;
 begin
-    -- Put a String
-    Put ("Enter an integer value: ");
-    -- Read in an integer value
-    Get (N);
-    Put (N);
+    loop
+        Put ("Enter an integer value: ");
+        Get (N);
+        Put (N);
 
-    if N = 0 or N = 360 then
-        Put_Line (" is due north");
-    elsif N in 1 .. 89 then
-        Put_Line (" is in the northeast quadrant");
-    elsif N = 90 then
-        Put_Line (" is due east");
-    elsif N in 91 .. 179 then
-        Put_Line (" is in the southeast quadrant");
-    elsif N = 180 then
-        Put_Line (" is due south");
-    elsif N in 181 .. 269 then
-        Put_Line (" is in the southwest quadrant");
-    elsif N = 270 then
-        Put_Line (" is due west");
-    elsif N in 271 .. 359 then
-        Put_Line (" is in the northwest quadrant");
-    else
-        Put_Line (" is not in the range 0 .. 360");
-    end if;
+        case N is
+            when 0 | 360 =>
+                Put_Line (" is due north");
+            when 1 .. 89 =>
+                Put_Line (" is in the northeast quadrant");
+            when 90 =>
+                Put_Line (" is due east");
+            when 91 .. 179 =>
+                Put_Line (" is in the southeast quadrant");
+            when 180 =>
+                Put_Line (" is due south");
+            when 181 .. 269 =>
+                Put_Line (" is in the southwest quadrant");
+            when 270 =>
+                Put_Line (" is due west");
+            when 271 .. 359 =>
+                Put_Line (" is in the northwest quadrant");
+            when others =>
+                Put_Line (" Au revoir");
+                exit;
+        end case;
+    end loop;
 end Check_Direction;
