78bb4fbe4bebf301cffb0618617622a4634b7849
PaulFSherwood <paulfsherwood@gmail.com>
2022-01-04 20:49:55 -0700

    finished with modulo

--- learnAda01/mod_demo.adb
+++ learnAda01/mod_demo.adb
@@ -3,8 +3,14 @@ use Ada.Text_IO;
 
 procedure Mod_Demo is
 	type Modular is mod 2**8;
+
+    M : Modular;
 begin
     Put_Line("Modular'First is " & Modular'Image(Modular'First));
     Put_Line("Modular'Last is " & Modular'Image(Modular'Last));
     Put_Line("Modular Mod is " & Integer'Image(Modular'Modulus));
+
+    -- M := Modular(1000 mod Integer(Modular'Modulus));
+    M := Modular'Mod(1000);
+    Put_Line("M is " & Modular'Image(M));
 end Mod_Demo;
