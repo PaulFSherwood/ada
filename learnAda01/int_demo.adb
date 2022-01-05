ca7b25bf4897231134d9cc6a6aaee204cda6214c
PaulFSherwood <paulfsherwood@gmail.com>
2022-01-04 17:50:53 -0700

    testing out modulo and if statements

--- learnAda01/int_demo.adb
+++ learnAda01/int_demo.adb
@@ -5,13 +5,27 @@ procedure Int_Demo is
 	A : Integer := 1;
 	
 	-- type Percent is range 1 .. 100;
-	type Percent is new Integer range 1 .. 100;
+	subtype Percent is Integer range 1 .. 100;
 	
 	P : Percent := 50;
+	
+	subtype Example is Integer range 50 .. 500;
+	
+	E : Example := 250;
 begin
 	Put_Line("A is " & Integer'Image(A));
 	Put_Line("First Integer is " & Integer'Image(Integer'First));
 	Put_Line("Last Integer is " & Integer'Image(Integer'Last));
-	-- Put_Line("P is " & Percent'Image(P));
-	Put_Line("P is " & Integer'Image(P));
+	Put_Line("P is " & Percent'Image(P));
+	-- Put_Line("P is " & Integer'Image(P));
+	Put_Line("E is " & Percent'Image(E));
+	E := P;
+	Put_Line("E is " & Percent'Image(E));
+	
+	for I in Percent'Range loop
+	-- for I in 2 .. 100 loop
+		if I mod 12 = 0 then
+			Put_Line("I is " & Integer'Image(I));
+		end if;
+	end loop;
 end Int_Demo;
