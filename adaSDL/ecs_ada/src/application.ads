7843aa1ab17a4831f9db2aaea77e3bd48e40909d
PaulFSherwood <paulfsherwood@gmail.com>
2026-01-28 17:23:20 -0700

    working ecs sdl ada test

--- adaSDL/ecs_ada/src/application.ads
+++
@@ -1,9 +0,0 @@
-with SDL.Video;
-with SDL.Render;
-
-package Application is
-   procedure Init;
-   procedure Run;
-   procedure Shutdown;
-end Application;
-
--- adaSDL/ecs_ada/src/main.adb
+++
@@ -1,9 +0,0 @@
-with Application;
-
-procedure Main is
-begin
-   Application.Init;
-   Application.Run;
-   Application.Shutdown;
-end Main;
-
