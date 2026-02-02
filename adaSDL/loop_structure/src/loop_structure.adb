c89daf46f4f97de4b6bdfd773623425e087fbec0
PaulFSherwood <paulfsherwood@gmail.com>
2026-02-02 16:33:47 -0700

    test change for loops

--- adaSDL/loop_structure/src/loop_structure.adb
+++ adaSDL/loop_structure/src/loop_structure.adb
@@ -1,22 +1,27 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Loop_Structure is
-   Start : Time := Clock;
    StartMs : Time;
    EndMs : Time;
    DelayMs : Time;
-   loopNumber : integer := 0;
-   FrameRate : integer := 0;
-   FrameMs : integer := 1000 / FrameRate;  --| Calculate the length of each frame
+   loopNumber : Integer := 0;
+   FrameRate : constant Integer := 60; --
+   FrameMs : constant Time_Span := To_Time_Span (1.0 / Float (FrameRate));
 begin
    loop
       StartMs := Clock;
       -- Update : Changes some state (a counter, position, etc)
       -- Render : Prints the current state
-      EndMs := Clock;
-      DelayMs := FrameMs - (EndMs - StartMs);
-      loop
-      exit when Clock >= DelayMs;
       Put_Line ("loop number: " & Integer'Image(loopNumber));
+      loopNumber := loopNumber + 1;
+      EndMs := Clock;
+
+      DelayMs := FrameMs - (EndMs - StartMs);
+
+      --| busy loop over having a delay
+      while Clock - StartMs < FrameMs loop
+         null;
+      end loop;
    end loop;
 end Loop_Structure;
