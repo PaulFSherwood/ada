f44fa737581f3849aa7dd478eec0789b2b3db1fe
PaulFSherwood <PaulFSherwood@gmail.com>
2026-02-04 12:04:39 -0700

    started seperating code into other files.

---
+++ adaSDL/sdl_loop/src/application.adb
@@ -0,0 +1,9 @@
+package body Application is
+   --  | Window setup
+   Screen_Height : constant := 800;
+   Screen_Width  : constant := 640;
+   --  | Window setup
+   Screen_Height : constant := 800;
+   Screen_Width  : constant := 640;
+
+end Application;
---
+++ adaSDL/sdl_loop/src/application.ads
@@ -0,0 +1,3 @@
+package Application is
+
+end Application;
