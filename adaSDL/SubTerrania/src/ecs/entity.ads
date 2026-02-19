31d2f6596959c240f372940ad59ad87c6bf5c739
PaulFSherwood <PaulFSherwood@gmail.com>
2026-02-19 11:15:47 -0700

    Working out ecs transition.

--- adaSDL/SubTerrania/src/ecs/entity.ads
+++ adaSDL/SubTerrania/src/ecs/entity.ads
@@ -1,1 +1,3 @@
-type Entity_ID is new Natrual;
+package ECS.Entity is
+   type Entity_ID is new Natural;
+end ECS.Entity;
--- adaSDL/SubTerrania/src/ecs/entity_manager.ads
+++ adaSDL/SubTerrania/src/ecs/entity_manager.ads
@@ -1,13 +1,25 @@
 with Transform;
+with ECS.Entity;
 
-package Entity_Manager is
-   procedure CreateEntity;
-   procedure StoreTransform
-      (XVal : Float;
-      YVal : Float;
-      T : in out Transform.Transform);
-   procedure StoreVelocity
-      (XVal : Float;
-      YVal : Float;
-      V : in Transform.Velocity);
-end Entity_Manager;
+package ECS.Entity_Manager is
+
+   function Create_Entity return ECS.Entity.Entity_ID;
+
+   procedure Add_Transform
+     (E : ECS.Entity.Entity_ID;
+      T : Transform.Transform);
+
+   procedure Add_Velocity
+     (E : ECS.Entity.Entity_ID;
+      V : Transform.Velocity);
+
+   function Get_Transform
+     (E : ECS.Entity.Entity_ID)
+      return Transform.Transform;
+
+   function Get_Velocity
+     (E : ECS.Entity.Entity_ID)
+      return Transform.Velocity;
+
+end ECS.Entity_Manager;
+
